193203cadae0dbe01f0640236e81a77d19b334d2
Malaika <laika.makapela18@gmail.com>
2023-06-18 19:10:32 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - Entry point
+  *Description: 'print all possible different combinations of two digits'
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	int d, p;
+
+	for (d = 0; p <= 9; d++)
+	{
+		for (p = d + 1; p <= 9; p++)
+		{
+			putchar((d % 10) + 0);
+			putchar((p % 10) + 0);
+
+			if (d == 8 && p == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
