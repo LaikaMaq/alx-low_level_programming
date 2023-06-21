4f507ef87809bfa6d869cbedce18cc6140798a2a
Malaika <laika.makapela18@gmail.com>
2023-06-20 17:20:27 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+  *main - prints the first 52
+  *Return: nothing
+  */
+
+int main(void)
+{
+
+	int i = 0;
+	long j = l;
+	long k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%d", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
